5138c505601a81f324635550b3a18fa178b9a414
homh9797 <harol.mont97@gmail.com>
2023-11-15 09:57:33 -0500

    Estructura inicial para el desarrollo del aplicativo

---
+++ api_web_servicio_bus/WebPagesAPI/WebPages/Controllers/ClientesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebPages.Controllers
+{
+    public class ClientesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var url = "";
+            return View();
+        }
+    }
+}
